9c3c8fb823085b429846afed883e1438f742b091
AdnanHafeez <adnanhr81@gmail.com>
2018-10-18 16:36:14 +1100

    Added final code

--- subarray.js
+++ subarray.js
@@ -1,17 +1,23 @@
 let maximum = (n1,n2) => {
   if(n1 === n2) return n1;
   if(n1 < n2) return n2;
-  return n3;
+  return n1;
 }
 
 var maxSubArray = function(nums) {
 
   if(nums.length < 1) return 0;
   let maxSum = nums[0];
-  let maxSumSoFar = num[0];
+  let maxSumSoFar = nums[0];
   for(let i = 1; i < nums.length; i++) {
-    maxSumSoFar = maximum(maxSumSoFar+nums[i],maxSumSoFar);
+    maxSumSoFar = maximum(maxSumSoFar+nums[i],nums[i]);
     maxSum = maximum(maxSumSoFar,maxSum);
   }
 
+  return maxSum;
+
 };
+
+let input = [-2,1,-3,4,-1,2,1,-5,4];
+
+console.log(maxSubArray(input));
